ea611cca0a2fde4f8b60827db1364c127ec23fd6
Maanas Nair <63333626+itsmebirdie@users.noreply.github.com>
2023-09-03 17:23:48 +0530

    pro: no of days in a month

---
+++ p4.py
@@ -0,0 +1,4 @@
+# A python program to create a dictionary whose keys are months names and values are number of days in that month. Ask the user to enter a month name and print the number of days in that month
+months = {'january':31,'february':28,'march':31,'april':30,'may':31,'june':30,'july':31,'august':31,'september':30,'october':31,'november':30,'december':31}
+month = input('Enter a month name: ')
+print('Number of days in ',month,' is ',months[month])
