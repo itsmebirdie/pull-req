1a6c66032ba36423623f15beec7738d8adc5cfbd
Maanas Nair <63333626+itsmebirdie@users.noreply.github.com>
2023-09-03 17:19:14 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+# A python program to find the average of three numbers
+n1 = int(input('Enter first number: '))
+n2 = int(input('Enter second number: '))
+n3 = int(input('Enter third number: '))
+avg = (n1+n2+n3)/3
+print('average: ',avg)
